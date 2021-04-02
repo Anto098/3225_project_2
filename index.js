86ec9752e79a96a897ae8e164533f1f9b473ee98
Anto <antonin.roy1@gmail.com>
2021-04-02 14:34:27 -0400

    prettified the page

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
-import { Instrument } from "/home/cyrille/node_modules/piano-chart";
+/* import { Instrument } from "/home/anto/node_modules/piano-chart";
 
 const piano = new Instrument(document.getElementById('pianoContainer'));
 piano.create();
 
 piano.keyDown("D4");
 piano.keyDown("F#4");
-piano.keyDown("A4");
+piano.keyDown("A4"); */
 
-console.log("Allo");
+function handleAnswer0(){
+    // get which game 
+    console.log("handling an answer : 0");
+}
+
+function handleAnswer1(){
+    // get which game 
+    console.log("handling an answer: 1");
+}
+
+
+
+function init(){
+    let buttons = document.querySelectorAll(".js-button");
+    for(let i = 0; i< buttons.length; i++){
+        let callback_method = "handleAnswer"+i;
+        buttons[i].addEventListener("click",callback_method);
+        // not sure if the callback method workaround works
+    }
+
+
+}
 
 window.addEventListener("load", init);
